fa1a3aa4d8852b00d11bab8709e36ce9a8318d5a
CassiniSea <78352344+CassiniSea@users.noreply.github.com>
2021-02-06 22:35:39 +0200

    Add Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,9 @@
-var uniqueInOrder=function(iterable){
+var uniqueInOrder = function(iterable){
 	return Array.from(iterable).join('').replace(/(.)\1+/g, '$1').split('').map(value => parseInt(value) ? parseInt(value) : value)
+}
+
+var uniqueInOrder2 = function(iterable){
+	return [...iterable].filter((a, i) => a !== iterable[i - 1])
 }
 
 console.log(uniqueInOrder('AAAABBBCCDAABBB'))
